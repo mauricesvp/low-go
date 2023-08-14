f1cd1724e50778dee8c13f7d744b48533b8959cd
mauricesvp <maurice@walny.de>
2023-08-15 01:01:41 +0200

    prettier with new config

--- options.js
+++ options.js
@@ -1,15 +1,15 @@
 function saveOptions(e) {
-  browser.storage.local.set({
-    viewCount: document.querySelector("#viewCount").value,
-  });
-  e.preventDefault();
+    browser.storage.local.set({
+        viewCount: document.querySelector("#viewCount").value,
+    });
+    e.preventDefault();
 }
 
 function restoreOptions() {
-  let gettingItem = browser.storage.local.get("viewCount");
-  gettingItem.then((res) => {
-    document.querySelector("#viewCount").value = res.viewCount || 100;
-  });
+    let gettingItem = browser.storage.local.get("viewCount");
+    gettingItem.then((res) => {
+        document.querySelector("#viewCount").value = res.viewCount || 100;
+    });
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
